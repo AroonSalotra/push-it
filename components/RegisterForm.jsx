493cd052746cde97b86016d9ee8a04ab68e834e3
Aroon Salotra <aroonsalotra@gmail.com>
2023-12-16 15:30:38 +0000

    add button css

---
+++ components/RegisterForm.jsx
@@ -0,0 +1,11 @@
+const RegisterForm = () => {
+    return ( 
+        <form action="" onSubmit={(e) => e.preventDefault()}>
+            <button className="btn-a">
+                Barbell
+            </button>
+        </form>
+     );
+}
+ 
+export default RegisterForm;
