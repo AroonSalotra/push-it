0f9e4895662cb7aa56af1bcd62ee256c9c4f0741
Aroon Salotra <aroonsalotra@gmail.com>
2023-12-16 15:56:50 +0000

    fix spacing for RegisterForm

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -2,7 +2,7 @@ import ButtonForm from "./ButtonForm";
 
 const RegisterForm = () => {
   return (
-    <form action="" onSubmit={(e) => e.preventDefault()}>
+    <form action="" onSubmit={(e) => e.preventDefault()} className="form-register">
       <ButtonForm body={"Barbell"} />
       <ButtonForm body={"Dumbell"} />
       <ButtonForm body={"Bodyweight"} />
