5f865e81d22ffe298abf6eed067a6a8f483c1756
Aroon Salotra <aroonsalotra@gmail.com>
2024-01-06 20:48:21 +0000

    fix .btn-a class width clipping

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -2,11 +2,16 @@ import ButtonForm from "./ButtonForm";
 
 const RegisterForm = () => {
   return (
-    <form action="" onSubmit={(e) => e.preventDefault()} className="form-register">
-      <ButtonForm body={"Barbell"} />
-      <ButtonForm body={"Dumbell"} />
-      <ButtonForm body={"Bodyweight"} />
-    </form>
+    <>
+      <form action="" onSubmit={(e) => e.preventDefault()}>
+        <fieldset className="form-container">
+          <legend className="form-title">Choose your plan</legend>
+          <ButtonForm body={"Barbell"} />
+          <ButtonForm body={"Dumbell"} />
+          <ButtonForm body={"Bodyweight"} />
+        </fieldset>
+      </form>
+    </>
   );
 };
 
